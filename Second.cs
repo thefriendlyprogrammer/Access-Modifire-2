4812bee2e3fa3fe01ad2880369533fd3d59dd61e
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-05-05 17:43:11 +0530

    Add files via upload

---
+++ Second.cs
@@ -0,0 +1,17 @@
+﻿using Access_modifire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifire2
+{
+    class Second
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+        }
+    }
+}
